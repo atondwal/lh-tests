80ba1dd2b07e9e1602ac2ca5a72c174044d292b0
Anish Tondwalkar <anish@archlinux.info>
2015-04-21 02:36:52 -0400

    currying bug in lh

--- mergesort.hs
+++ mergesort.hs
@@ -2,17 +2,21 @@
 
 {-@ mergesort :: (Ord a) => [a] -> IncrList a @-}
 mergesort :: Ord a => [a] -> [a]
-mergesort = (uncurry merge) . split
+mergesort = (uncurry conquer) . divide
 
-merge :: Ord a => [a] -> [a] -> [a]
-merge [] [] = []
-merge [x] [] = [x]
-merge [] [y] = [y]
-merge (x:xs) (y:ys) = if x>y then x:merge xs (y:ys) else y:merge (x:xs) ys
+{-@ conquer :: (Ord a) => IncrList a -> IncrList a -> IncrList a @-}
+conquer :: Ord a => [a] -> [a] -> [a]
+conquer [] [] = []
+conquer [x] [] = [x]
+conquer [] [y] = [y]
+conquer (x:xs) (y:ys) = if x>y then x:conquer xs (y:ys) else y:conquer (x:xs) ys
 
-split :: Ord a => [a] -> ([a],[a])
-split [] = ([],[])
-split [a] = ([a],[])
-split [a,b] = ([a],[b])
-split (x:y:zs) = (mergesort (x:xs), mergesort (y:ys))
-  where (xs,ys) = split zs
+{-@ divide :: (Ord a) => [a] -> IncrList a -> IncrList a @-}
+divide :: Ord a => [a] -> ([a],[a])
+divide l = (mergesort xs, mergesort ys)
+  where (xs,ys) =  split l
+        split [] = ([],[])
+        split [a] = ([a],[])
+        split (x:y:zs) = (x:χs,y:ηs)
+          where (χs,ηs) = split zs
+
