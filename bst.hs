7bdedbeee53c1fe8265ee1ec824dba69439266a5
Anish Tondwalkar <anish@archlinux.info>
2015-04-23 00:00:11 -0400

    search test cases

--- bst.hs
+++ bst.hs
@@ -14,13 +14,19 @@ balanced :: Node a -> Bool
 balanced (Leaf _) = True
 balanced (Node r _ l) = balanced r && balanced l && (height r) - (height l) < 1 && (height r) - (height l) > -1
 
+{-@ testTreeEven :: {v : Node Integer | balanced v} @-}
+testTreeEven :: Node Integer
+testTreeEven = Node (Leaf 1) 2 (Leaf 3)
 
 {-@ type BST a = Node <{\b v n -> (n<=v) <=> b==True}> a @-}
+{-@ testTreeLeft :: {v : Node Integer | balanced v} @-}
+testTreeLeft :: Node Integer
+testTreeLeft = Node (Node (Leaf 0) 1 (Leaf 3)) 2 (Leaf 3)
 
-{-@ testTree :: BST Integer @-}
-testTree :: Node Integer
-testTree = Node (Leaf 1) 2 (Leaf 3)
+{-@ testTreeRight :: {v : Node Integer | balanced v} @-}
+testTreeRight :: Node Integer
+testTreeRight = Node (Leaf 3) 2 (Node (Leaf 0) 1 (Leaf 3))
 
-{-@ testTree2 :: BST Integer @-}
-testTree2 :: Node Integer
-testTree2 = Node (Leaf 5) 2 (Leaf 3)
+{-@ testTreeBroke :: {v : Node Integer | balanced v} @-}
+testTreeBroke :: Node Integer
+testTreeBroke = Node (Node (Node (Leaf 0) 1 (Leaf 3)) 3 (Node (Leaf 0) 1 (Leaf 3))) 1 (Leaf 3)
