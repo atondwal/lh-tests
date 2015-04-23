b618f79cabef7c09f07ee53070409ccdef33e5c8
Anish Tondwalkar <anish@archlinux.info>
2015-04-23 13:40:35 -0400

    unsearch

--- bst.hs
+++ bst.hs
@@ -1,31 +1,31 @@
-{-@ data Node v = Leaf v | Node (r :: Node {a:v|a<c} ) (c :: v) (l :: Node {a:v|a>c} ) @-}
+{-@ data Node v = Leaf v | Node (c :: v) (r :: Node {a:v|a<c} ) (l :: Node {a:v|a>c} ) @-}
 
-data Node v = Leaf v | Node (Node v) v (Node v)
+data Node v = Leaf v | Node v (Node v) (Node v)
 
 {-@ measure height @-}
 height :: Node a -> Integer
 height (Leaf _) = 1
-height (Node r _ l) = if ρ > λ then ρ else λ
+height (Node _ r l) = if ρ > λ then ρ else λ
         where ρ = 1 + height r
               λ = 1 + height l
 
 {-@ measure balanced @-}
 balanced :: Node a -> Bool
 balanced (Leaf _) = True
-balanced (Node r _ l) = balanced r && balanced l && (height r) - (height l) <= 1 && (height r) - (height l) >= -1
+balanced (Node _ r l) = balanced r && balanced l && (height r) - (height l) <= 1 && (height r) - (height l) >= -1
 
 {-@ testTreeEven :: {v : Node Integer | balanced v} @-}
 testTreeEven :: Node Integer
-testTreeEven = Node (Leaf 1) 2 (Leaf 3)
+testTreeEven = Node 2 (Leaf 1) (Leaf 3)
 
 {-@ testTreeLeft :: {v : Node Integer | balanced v} @-}
 testTreeLeft :: Node Integer
-testTreeLeft = Node (Node (Leaf 0) 1 (Leaf 3)) 2 (Leaf 3)
+testTreeLeft = Node 2 (Node 1 (Leaf 3) (Leaf 0)) (Leaf 3)
 
 {-@ testTreeRight :: {v : Node Integer | balanced v} @-}
 testTreeRight :: Node Integer
-testTreeRight = Node (Leaf 3) 2 (Node (Leaf 0) 1 (Leaf 3))
+testTreeRight = Node 1 (Leaf 3) (Node 2 (Leaf 0) (Leaf 3))
 
 {-@ testTreeBroke :: {v : Node Integer | balanced v} @-}
 testTreeBroke :: Node Integer
-testTreeBroke = Node (Node (Node (Leaf 0) 1 (Leaf 3)) 3 (Node (Leaf 0) 1 (Leaf 3))) 1 (Leaf 3)
+testTreeBroke = Node 1 (Node 1 (Leaf 3) (Node 1 (Leaf 0) (Leaf 3))) (Node 3 (Leaf 0) (Leaf 3))
